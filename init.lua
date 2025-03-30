8793c61e2f3364776516df252916ffb26a2073e4
phellype07 <prff97@gmail.com>
2025-03-30 15:38:55 -0300

    Change in python formatters

--- init.lua
+++ init.lua
@@ -716,7 +716,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'ruff' },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
